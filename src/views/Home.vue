127c483c37309ce1a1d2178d4673a5dce96564e1
Lauren Miller <lomiller0913@gmail>
2021-04-19 20:21:25 -0400

    adds bootstrap and resume styling, home and resume views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <p>{{firstName}} {{lastName}}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      firstName: "Stevie",
+      lastName: "Wonder",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
 </script>
---
+++ src/views/Resume.vue
@@ -0,0 +1,39 @@
+
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p></p>
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "View this resume",
+      firstName: "Stevie",
+      lastName: "Wonder",
+      email: "stevie@email.com",
+      phoneNumber: "5555555555",
+      shortBio: "Stevie Wonder is dope",
+      linkedInURL: "url here",
+      twitterHandle: "twitter handle here",
+      personaWebsiteURL: "url here",
+      onlineResumeURL: "url",
+      githubURL: "url",
+      photo: "image",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
+
+
+
