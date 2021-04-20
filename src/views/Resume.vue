ab3f761dd775930521148941f8d199c7ab1293de
Mat <mvello66@gmail.com>
2021-04-20 19:56:02 -0400

    makes student.skills dynamic in student resume (without backend)

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -111,6 +111,7 @@
                 <div class="resume-section-content">
                     <h2 class="mb-5">Skills</h2>
                     <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <p> </p>
                     <!-- <ul class="list-inline dev-icons">
                         <li class="list-inline-item"><i class="fab fa-html5"></i></li>
                         <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
@@ -126,22 +127,10 @@
                         <li class="list-inline-item"><i class="fab fa-npm"></i></li>
                     </ul> -->
                     <div class="subheading mb-3">Workflow</div>
-                    <ul class="fa-ul mb-0">
+                    <ul class="fa-ul mb-0" v-for="skill in student.skills">
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Mobile-First, Responsive Design
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Functional Teams
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Agile Development & Scrum
+                            {{ skill }} 
                         </li>
                     </ul>
                 </div>
